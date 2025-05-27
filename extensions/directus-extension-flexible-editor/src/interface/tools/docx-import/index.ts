2c7e7e0aeb183d8df80ece2d7ebc3d905c1d8041
pixelsock <nick@stump.works>
2025-05-26 21:32:58 -0400

    Add docx-import tool

---
+++ extensions/directus-extension-flexible-editor/src/interface/tools/docx-import/index.ts
@@ -0,0 +1,14 @@
+import DocxImport from '@custom-editor/docx-import';
+import { defineTool } from '../lib';
+import customMessages from '../i18n/custom-messages';
+import type { Editor } from '@tiptap/core';
+
+export default defineTool({
+    key: 'docxImport',
+    name: customMessages.tools.docx_import,
+    icon: 'upload_file',
+    extension: [DocxImport],
+    action: (editor: Editor) => editor.chain().focus().docxImport().run(),
+    disabled: (editor: Editor) => !editor.can().chain().focus().docxImport().run(),
+    active: () => false,
+});
